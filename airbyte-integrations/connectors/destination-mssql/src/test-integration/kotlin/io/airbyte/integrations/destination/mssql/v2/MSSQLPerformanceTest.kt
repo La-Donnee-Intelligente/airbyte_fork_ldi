airbytehq/airbyte

tmp tuning knobs

airbyte-integrations/connectors/destination-mssql/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLPerformanceTest.kt
@@ -53,11 +53,12 @@ abstract class MSSQLPerformanceTest(
         configSpecClass = MSSQLSpecification::class.java,
         configUpdater = configUpdater,
         dataValidator = MSSQLDataValidator(getConfiguration),
-        defaultRecordsToInsert = 10000,
+        defaultRecordsToInsert = 1_000_000,
+        numStreamsForMultiStream = 20
     ) {
     @Test
     override fun testInsertRecords() {
-        testInsertRecords(recordsToInsert = 100000) {}
+        testInsertRecords(recordsToInsert = 1_000_000) {}
     }
 
     @Test

La-Donnee-Intelligente/airbyte_fork_ldi

airbyte-integrations/connectors/destination-mssql/src/test-integration/kotlin/io/airbyte/integrations/destination/mssql/v2/MSSQLPerformanceTest.kt
@@ -53,11 +53,12 @@ abstract class MSSQLPerformanceTest(
         configSpecClass = MSSQLSpecification::class.java,
         configUpdater = configUpdater,
         dataValidator = MSSQLDataValidator(getConfiguration),
-        defaultRecordsToInsert = 10000,
+        defaultRecordsToInsert = 1_000_000_000_000,
+        numStreamsForMultiStream = 20
     ) {
     @Test
     override fun testInsertRecords() {
-        testInsertRecords(recordsToInsert = 100000) {}
+        testInsertRecords(recordsToInsert = 1_000_000_000_000) {}
     }
 
     @Test